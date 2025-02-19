2f6c74ecd5999c633b6c1b42a8c01f16e6a41385
MbaHyo <grandmaster1163@gmail.com>
2025-02-19 23:35:31 +0700

    Padukuhan Ngrancang
    unfinished

---
+++ README.md
@@ -0,0 +1,2 @@
+# padukuhan-ngrancang
+ 
---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    let menu = document.querySelector(".nav-links");
+    let toggle = document.querySelector(".menu-toggle");
+    menu.classList.toggle("active");
+    toggle.classList.toggle("active");
+}   
