b4ede8d51c3a7318b86f8c0a6268ad4ad2e78e30
MbaHyo <70987885+MbaHyo@users.noreply.github.com>
2025-02-20 06:45:40 +0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() {
-    let menu = document.querySelector(".nav-links");
-    let toggle = document.querySelector(".menu-toggle");
-    menu.classList.toggle("active");
-    toggle.classList.toggle("active");
-}   
